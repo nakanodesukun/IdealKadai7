d9cfee0926bf76c9083a22af542830197433ad86
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-01-06 22:16:50 +0900

    Initial Commit

---
+++ IdealKadai7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdealKadai7
+//
+//  Created by 中野翔太 on 2022/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
